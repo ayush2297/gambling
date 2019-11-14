5c4a42ddc71c74323a4dec6954ef923245b4868a
ayush2297 <ayush.saraf47@gmail.com>
2019-11-14 11:38:11 +0530

    calculative gambler approach implemented who sets goals to quit the game

--- gambling.sh
+++ gambling.sh
@@ -1,9 +1,24 @@
-# /bin/bash -x
+#! /bin/bash -x
 
+#constants
 declare BET_AMOUNT=1
 declare WIN=1
+declare GOAL=0
+declare BROKE=$(( $stake-$(($stake/2)) ))
+declare DAILY_INITIAL_STAKE=100
+declare INFINITE_LOOP=1
+declare ENOUGH_FOR_TODAY=0
 
-declare stake=100
+#variables
+declare stake=0
+
+function setGoal(){
+	GOAL=$(( $stake+$(($stake/2)) ))
+}
+
+function setBroke(){
+   BROKE=$(( $stake-$(($stake/2)) ))
+}
 
 function bet(){  
    betResult=$((RANDOM%2))
@@ -16,4 +31,34 @@ function bet(){
 	fi
 }
 
-bet
+function checkResign(){
+	if [ $stake -ge $GOAL ]
+	then
+		echo "goal reached..... enough for today "
+		ENOUGH_FOR_TODAY=1
+	elif [ $stake -le $BROKE ]
+	then
+		echo "lost too much money...enough for today"
+		ENOUGH_FOR_TODAY=1
+	fi
+}
+
+stake=$DAILY_INITIAL_STAKE
+setGoal
+setBroke
+echo "DAY : " $day
+echo "GOAL : " $GOAL
+echo "LEAVE AT: "$BROKE
+echo "day begins with stake : "$stake
+while [ $INFINITE_LOOP -eq 1 ]
+do
+	bet
+	count=$(($count+1))
+	echo $stake
+	if [ $ENOUGH_FOR_TODAY == 1 ]
+	then
+		ENOUGH_FOR_TODAY=0
+		break
+	fi
+done
+echo "day ends with $stake"
